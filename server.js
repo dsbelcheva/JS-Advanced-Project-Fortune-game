14d3a89f1af4e8749c22fc95ca48da8253d8d7bb
Duygu Hasan <duyguhsnhsn@gmail.com>
2022-12-06 17:07:33 +0200

    Removing the login signup materials for now since they are not linked to the main functionality

--- server.js
+++ server.js
@@ -3,22 +3,5 @@ const app = express()
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/views/index.html'));
-});
-
-
-router.get('/login',function(req,res){
-    res.sendFile(path.join(__dirname+'/views/log_in.html'));
-});  
-
-router.get('/default',function(req,res){
-    res.sendFile(path.join(__dirname+'/views/default_version.html'));
-});  
-
-router.get('/signup',function(req,res){
-    res.sendFile(path.join(__dirname+'/views/sign_up.html'));
-});
-
 app.use('/', router);
 app.listen(3000)
