15ca7fb55bef8f05b42fb219f9580d708813074c
Duygu Hasan <duyguhsnhsn@gmail.com>
2022-12-07 18:35:54 +0200

    Add posts rooms section

---
+++ public/main_script.js
@@ -0,0 +1,24 @@
+const socket = io('http://localhost:3000')
+const roomContainer = document.getElementById('room-container')
+
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement('a')
+  roomLink.href = `/${room}`
+  roomLink.innerText = 'join'
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
