a102dea2cbd5f9afac3f7fa35593f28837db174b
Desislava Belcheva <72351356+dsbelcheva@users.noreply.github.com>
2022-12-08 15:22:06 +0200

    Adding code for rooms

--- public/main_script.js
+++ public/main_script.js
@@ -1,5 +1,22 @@
 const socket = io('http://localhost:3000')
 const roomContainer = document.getElementById('room-container')
+const notificationContainer = document.getElementById('notification-container')
+const notificationInput = document.getElementById('notification-input') 
+const postForm = document.getElementById('post-container')
+
+if (postForm != null) {
+  const user_name = prompt('What is your name?')
+  appendMessage('You joined successfully')
+  socket.emit('new-user', roomName, user_name )
+
+  postForm.addEventListener('submit', eventt => {
+    eventt.preventDefault()
+    const message = notificationInput.value
+    appendMessage('You: ${message}')
+    socket.emit('send-notification-message', roomName, message)
+    notificationInput.value=''
+  } )
+}
 
 socket.on('room-created', room => {
   const roomElement = document.createElement('div')
@@ -22,3 +39,9 @@ socket.on('user-connected', name => {
 socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
+
+function appendMessage (message) {
+  const messageEl = document.createElement('div')
+  messageEl.innerText = message
+  notificationContainer.append(messageEl)
+}
